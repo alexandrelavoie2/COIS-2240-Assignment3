6f6b9b6659615723290a47a47ac26206f6d71d81
Alexandre Lavoie <alexandrelavoie2@gmail.com>
2026-03-30 02:21:27 -0400

    Task 3 (OPTIONAL): RentalSystemGUI, Main Menu is created. Buttons don't work yet.

--- RentalProject/src/RentalSystemGUI.java
+++ RentalProject/src/RentalSystemGUI.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class RentalSystemGUI extends Application {
@@ -12,12 +15,41 @@ public class RentalSystemGUI extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label lblMessage = new Label("Vehicle Rental System GUI");
-        StackPane spRoot = new StackPane(lblMessage);
-        Scene scene = new Scene(spRoot, 500, 300);
+        VBox vbRoot = new VBox(14);
+        vbRoot.setPadding(new Insets(24));
+        vbRoot.setAlignment(Pos.CENTER);
 
+        Button btnAddVehicle = createMenuButton("1: Add Vehicle");
+        Button btnAddCustomer = createMenuButton("2: Add Customer");
+        Button btnRentVehicle = createMenuButton("3: Rent Vehicle");
+        Button btnReturnVehicle = createMenuButton("4: Return Vehicle");
+        Button btnDisplayAvailable = createMenuButton("5: Display Available Vehicles");
+        Button btnShowHistory = createMenuButton("6: Show Rental History");
+        Button btnExit = createMenuButton("0: Exit");
+
+        btnExit.setOnAction(event -> stage.close());
+
+        vbRoot.getChildren().addAll(
+                btnAddVehicle,
+                btnAddCustomer,
+                btnRentVehicle,
+                btnReturnVehicle,
+                btnDisplayAvailable,
+                btnShowHistory,
+                btnExit);
+
+        Scene scene = new Scene(vbRoot, 700, 650);
         stage.setTitle("Vehicle Rental System GUI");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private Button createMenuButton(String sText) {
+        Button btnMenu = new Button(sText);
+        btnMenu.setMaxWidth(Double.MAX_VALUE);
+        btnMenu.setMaxHeight(Double.MAX_VALUE);
+        btnMenu.setStyle("-fx-font-size: 22px; -fx-font-weight: bold;");
+        VBox.setVgrow(btnMenu, Priority.ALWAYS);
+        return btnMenu;
     }
 }
